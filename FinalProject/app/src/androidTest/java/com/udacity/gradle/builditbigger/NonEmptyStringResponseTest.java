aca3c004f79965eef56cb5c2209c6429a3c515b6
aanandshekharroy <aanandshekharroy@gmail.com>
2016-11-09 03:28:51 +0530

    feat:Functional tests done

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringResponseTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/NonEmptyStringResponseTest.java
@@ -1,21 +1,52 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.support.v4.util.Pair;
 import android.test.AndroidTestCase;
+
+import org.mockito.Mock;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by theseus on 8/11/16.
  */
 
 public class NonEmptyStringResponseTest extends AndroidTestCase {
-    public void test(){
-        String result=null;
-        EndpointsAsyncTask endpointsAsyncTask=new EndpointsAsyncTask(getContext(),null);
-        endpointsAsyncTask.execute();
-        try {
-            result=endpointsAsyncTask.get();
+
+    EndpointsAsyncTask task;
+    String result;
+    @Mock
+    Context mockContext;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        result = null;
+        task = new EndpointsAsyncTask(mockContext,null){
+            @Override
+            protected void onPostExecute(String joke){
+                //No need to launch intent, so override this method
+                return ;
+            }
+        };
+    }
+
+
+    public void testAsyncReturnType() {
+
+        try{
+
+            //Default timeout for the GCM server is 20 seconds
+            //If the .get can't get the result in 10 seconds, something is wrong anyway
+            //Greater than 20 seconds results in an error string returned and requires further interpretation
+            task.execute(new Pair<Context, String>(mockContext, "Manfred"));
+            result = task.get(10, TimeUnit.SECONDS);
+            assertNotNull(result);
+
         }catch (Exception e){
-            e.printStackTrace();
+            fail("Timed out");
         }
-        assertNotNull(result);
     }
 }
